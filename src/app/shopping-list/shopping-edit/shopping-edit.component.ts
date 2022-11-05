36c72d2650fccdfac2d7bf56b85981ae83ec6f09
DT0998 <duongkytrieuvn@gmail.com>
2022-11-05 22:36:18 +0700

    feat: add smth

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,9 +1,19 @@
-import {Component} from '@angular/core'
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core'
+import { Ingredient } from 'src/app/shared/ingredient.model';
 @Component({
     selector: 'app-shopping-edit',
     templateUrl: './shopping-edit.component.html',
     styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent  {
-    
+    // ref to element in template
+    @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
+    @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
+    @Output() ingredientAdded = new EventEmitter<Ingredient>();
+    onAddItem() {
+        const ingredientName = this.nameInputRef.nativeElement.value;
+        const ingredientAmount = this.amountInputRef.nativeElement.value;
+        const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+        this.ingredientAdded.emit(newIngredient);
+    }
  }
