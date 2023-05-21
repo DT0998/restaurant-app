1abe0bacc2d854c0ed1b04f14725a1ec2c902afc
DT0998 <duongkytrieuvn@gmail.com>
2023-05-21 20:13:08 +0700

    feat: add smth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,9 +6,10 @@ import { Component, EventEmitter, Output } from "@angular/core";
     templateUrl: './header.component.html',
 })
 export class HeaderComponent {
- @Output() featureSelected = new EventEmitter<string>();   
-  onSelect(feature: string) {
-  //  use str to navigate to the right component in app.component.html
-    this.featureSelected.emit(feature);
-  }
+//  @Output() featureSelected = new EventEmitter<string>();
+//   onSelect(feature: string) {
+//   //  use str to navigate to the right component in app.component.html
+//     this.featureSelected.emit(feature);
+//   }
+  
 }
