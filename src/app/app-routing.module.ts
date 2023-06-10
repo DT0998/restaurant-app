68a610f79d8253875ed4b0420709af935bcccfe5
DT0998 <duongkytrieuvn@gmail.com>
2023-06-11 01:12:05 +0700

    feat: add smth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 
 const appRoutes: Routes = [
   {
@@ -12,6 +13,12 @@ const appRoutes: Routes = [
   {
     path: "recipes",
     component: RecipesComponent,
+    children: [
+      {
+        path: "",
+        component: RecipeStartComponent,
+      },
+    ],
   },
   {
     path: "shopping-list",
